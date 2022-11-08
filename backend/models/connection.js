9ab99a9e972c78b9d24e1d66bb2593942265b5ad
dima <hibdhz221@gmail.com>
2022-11-09 01:46:15 +0200

    wip

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 async function ConnectDB({DB_path}) {
-  const connection = await mongoose.connect(`mongodb://localhost:27017/${DB_name}`);
+  const connection = await mongoose.connect(DB_path);
   return connection;
 }
 
