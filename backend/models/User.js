28987021b7ac56cc35ebd8737209f6e5890c0b07
dima <hibdhz221@gmail.com>
2022-11-09 01:40:00 +0200

    wip

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,22 +1,30 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
+import autoIncrementFactory from 'mongoose-sequence';
+import { SALT_WORK_FACTOR } from '../settings/dbConfig.js';
 
-const SALT_WORK_FACTOR = 10; 
+
+const autoIncrement = autoIncrementFactory(mongoose);
 
 const UserSchema = new mongoose.Schema({
+    _id: Number,
     name: String,
     lastname: String,
     password: String,
-    type: String, 
-});
+    type: String,
+
+}, { _id: false });
+
+
+UserSchema.plugin(autoIncrement, { inc_field: '_id' });
 
 
 UserSchema.pre('save', function(next){
     const user = this 
-    if (user.password === undefined) {
-        return next()
+    if (user.password === undefined) { // if there is no password throw err
+        return next();
     };
-
+    
     bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
         if (err) console.log(err);
         // hash the password using our new salt
@@ -27,8 +35,6 @@ UserSchema.pre('save', function(next){
         });
     });
 });
-
-
 
 const User = mongoose.model('Users', UserSchema);
 export default User;
